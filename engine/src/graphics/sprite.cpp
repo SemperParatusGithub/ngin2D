a8e989913075f2d1985244488ddc69ac9fb85b50
SemperParatusGithub <schlagernils@gmail.com>
2026-03-27 15:00:35 +0100

    Change Transform to lowercase

---
+++ engine/src/graphics/sprite.cpp
@@ -0,0 +1,30 @@
+#include "sprite.h"
+
+
+namespace ngin {
+
+Sprite::Sprite() {
+}
+
+Sprite::Sprite(ref<Texture> texture) : 
+	m_texture(texture) {
+	recalculate_texture_coords();
+}
+
+Sprite::~Sprite() {
+}
+
+void Sprite::set_texture(ref<Texture> texture) {
+	m_texture = texture;
+
+	recalculate_texture_coords();
+}
+
+void Sprite::recalculate_texture_coords() {
+	m_texture_coords[0] = { 0.0f, 0.0f };
+	m_texture_coords[1] = { 1.0f, 0.0f };
+	m_texture_coords[2] = { 1.0f, 1.0f };
+	m_texture_coords[3] = { 0.0f, 1.0f };
+}
+
+} // namespace ngin
